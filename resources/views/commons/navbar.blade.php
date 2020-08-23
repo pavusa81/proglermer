6ca94acecaa923583ff14221c9c438f3b1e4214c
Souhei.Konishi <reepoo0528@outlook.jp>
2020-08-23 10:25:03 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="mb-1">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <a href="#" class="navbar-brand logo-small">Prog-leamer</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="nav-bar">
+                    <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="navbar-nav navi-white">
+                        <li class="nav-item mr-4">新規登録</li>
+                        <li class="nav-item mr-4">ログイン</li>
+                        <li class="nav-item mr-4">学習者一覧</li>
+                        <li class="nav-item mr-4">学習記録一覧</li>
+                    </ul>
+                </div>
+        </nav>
+</header>
